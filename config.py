2f2db7a307703ec0e1063b1b0c8c3a56e28c5e38
hutaobest <lelouchinuzuma@gmail.com>
2025-02-17 11:37:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/hutaobest/janedoebitch1",
+    "https://github.com/hutaobest/janedoebitch2",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
